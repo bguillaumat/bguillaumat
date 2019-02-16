8de82abab2fd29e65a259a21bf901eff8df168db
bguillaumat <bastien.guillaumat@epitech.eu>
2019-02-16 22:22:26 +0100

    Add homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: 'full'},
+  {path: '**', redirectTo: ''},
+  // {path: '**', component: NotFoundComponent, pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
