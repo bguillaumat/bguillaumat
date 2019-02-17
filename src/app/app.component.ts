3c6681bdfd8a5de27942f62def93b65e8e93fe31
bguillaumat <bastien.guillaumat@epitech.eu>
2019-02-17 15:26:38 +0100

    Update CV part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +9,15 @@ export class AppComponent {
   // title = 'BGuillaumat';
 
   opened = true;
+  isMobile = false;
+
+  @HostListener('window:resize', ['$event']) onResize(event) {
+    this.isMobile = event.target.innerWidth <= 900;
+    this.opened = !this.isMobile;
+  }
+
+  constructor() {
+    this.isMobile = (window.innerWidth < 900);
+    this.opened = !this.isMobile;
+  }
 }
