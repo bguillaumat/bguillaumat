1649657849c1e5a896a9ff7dddbbaed30f06304a
bguillaumat <bastien.guillaumat@epitech.eu>
2019-02-19 02:40:43 +0100

    Update landing

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  year = new Date().getFullYear();
+  month = new Date().getMonth() + 1;
+  day = new Date().getDate();
+  yo = 1998;
+
+  constructor() {
+    this.yo = this.year - this.yo;
+    if (this.month <= 2 && this.day <= 26) {
+      this.yo -= 1;
+    }
+  }
 
   ngOnInit() {
   }
