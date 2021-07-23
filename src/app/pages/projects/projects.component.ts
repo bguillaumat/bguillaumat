9db3b6357cef7afac822527f0fe8c19c60294d37
bguillaumat <bastien.guillaumat@gmail.com>
2021-07-23 14:07:57 +0200

    Update datas

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -11,6 +11,12 @@ export class ProjectsComponent implements OnInit {
 
   constructor() {
     this.projects.push({
+      name: 'SQUADS',
+      year: '2021',
+      content: 'Decentralized application for creation of better, simpler DAOs. Built on Solana Blockchain',
+      languages: [{name: 'TypeScript', css: 'ts'}, {name: 'Rust', css: 'rust'}]
+    });
+    this.projects.push({
       name: 'Wav Header Parser',
       year: '2020',
       content: 'NPM module made for parse WAV files header',
